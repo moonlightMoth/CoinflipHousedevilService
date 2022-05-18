0e4cbd60afbb01e64b440183bb96fead6eee7396
moonlightMoth <jyhgftm@gmail.com>
2022-05-19 00:56:20 +0300

    test commit

--- src/main/java/moonlightmoth/housedevil/coinflip/CoinflipApplication.java
+++ src/main/java/moonlightmoth/housedevil/coinflip/CoinflipApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+
 public class CoinflipApplication {
 
 	public static void main(String[] args) {
